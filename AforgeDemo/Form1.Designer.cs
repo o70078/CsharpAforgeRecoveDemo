30d3e9c99a391c57ebcfab435b15a1365abe6530
asml <asml@ASMLCOMPUTER>
2017-09-20 19:43:23 +0800

    添加项目文件。

---
+++ AforgeDemo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AforgeDemo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 50;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(47, 37);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(690, 402);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(783, 581);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ AforgeDemo/Form1.cs
@@ -0,0 +1,90 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AforgeDemo
+{
+	public partial class Form1 : Form
+	{
+		//用来操作摄像头
+		private VideoCaptureDevice Camera = null;
+		//用来把每一帧图像编码到视频文件
+		private VideoFileWriter VideoOutPut = new VideoFileWriter();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			pictureBox1.Refresh();
+		}
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			//获取摄像头列表
+			var devs = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+			//实例化设备控制类
+			Camera = new VideoCaptureDevice(devs[0].MonikerString);
+
+			//配置录像参数(宽,高,帧率,比特率,等)
+			Camera.VideoResolution = Camera.VideoCapabilities[0];
+
+			//设置回调,aforge会不断从这个回调推出图像数据
+			Camera.NewFrame += Camera_NewFrame;
+
+			//开始
+			Camera.Start();
+
+			//打开录像文件(如果没有则创建,如果有也会清空).
+			VideoOutPut.Open("E:/VIDEO.MP4", Camera.VideoResolution.FrameSize.Width, Camera.VideoResolution.FrameSize.Height, Camera.VideoResolution.AverageFrameRate, VideoCodec.MPEG4, Camera.VideoResolution.BitCount);
+		}
+
+		//图像缓存
+		private Bitmap bmp = new Bitmap(1, 1);
+		////摄像头输出回调
+		private void Camera_NewFrame(object sender, NewFrameEventArgs eventArgs)
+		{
+			//写到文件
+			VideoOutPut.WriteVideoFrame(eventArgs.Frame);
+			lock (bmp)
+			{
+				//释放上一个缓存
+				bmp.Dispose();
+				//保存一份缓存
+				bmp = eventArgs.Frame.Clone() as Bitmap;
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			//停摄像头
+			Camera.Stop();
+
+			//关闭录像文件,如果忘了不关闭,将会得到一个损坏的文件,无法播放
+			VideoOutPut.Close();
+		}
+
+		private void pictureBox1_Paint(object sender, PaintEventArgs e)
+		{
+			if (bmp == null) return;
+
+			lock (bmp) e.Graphics.DrawImage(bmp, 0, 0);
+		}
+	}
+}
